a1ad8837fd2e7716dee7cf37462e1a04aea006ca
Aakash Sharma <aakash.y.sharma@gmail.com>
2015-06-20 15:41:57 +0200

    Added some more skeleton classes

--- UPartyPlayer/src/main/java/de/upp/meta/TrackMetaInfo.java
+++ UPartyPlayer/src/main/java/de/upp/meta/TrackMetaInfo.java
@@ -23,6 +23,90 @@ public class TrackMetaInfo implements Searchable{
 	
 	
 
+	/**
+	 * @return the trackID
+	 */
+	public String getTrackID() {
+		return trackID;
+	}
+
+	/**
+	 * @param trackID the trackID to set
+	 */
+	public void setTrackID(String trackID) {
+		this.trackID = trackID;
+	}
+
+	/**
+	 * @return the trackName
+	 */
+	public String getTrackName() {
+		return trackName;
+	}
+
+	/**
+	 * @param trackName the trackName to set
+	 */
+	public void setTrackName(String trackName) {
+		this.trackName = trackName;
+	}
+
+	/**
+	 * @return the trackArtist
+	 */
+	public TrackArtist getTrackArtist() {
+		return trackArtist;
+	}
+
+	/**
+	 * @param trackArtist the trackArtist to set
+	 */
+	public void setTrackArtist(TrackArtist trackArtist) {
+		this.trackArtist = trackArtist;
+	}
+
+	/**
+	 * @return the playBackTime
+	 */
+	public Time getPlayBackTime() {
+		return playBackTime;
+	}
+
+	/**
+	 * @param playBackTime the playBackTime to set
+	 */
+	public void setPlayBackTime(Time playBackTime) {
+		this.playBackTime = playBackTime;
+	}
+
+	/**
+	 * @return the yearOfRelease
+	 */
+	public int getYearOfRelease() {
+		return yearOfRelease;
+	}
+
+	/**
+	 * @param yearOfRelease the yearOfRelease to set
+	 */
+	public void setYearOfRelease(int yearOfRelease) {
+		this.yearOfRelease = yearOfRelease;
+	}
+
+	/**
+	 * @return the genre
+	 */
+	public String getGenre() {
+		return genre;
+	}
+
+	/**
+	 * @param genre the genre to set
+	 */
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
 	@Override
 	public boolean isSameAs(Searchable otherTrack) {
 		// TODO Auto-generated method stub
---
+++ UPartyPlayer/src/main/java/de/upp/player/TrackPool.java
@@ -0,0 +1,14 @@
+package de.upp.player;
+
+import java.util.ArrayList;
+
+/**
+ * Implements pool of possible next tracks based on users available at party and
+ * other contextual factors at that time
+ * 
+ * @author aakash
+ *
+ */
+public class TrackPool {
+
+}
