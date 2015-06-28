96b4a71ab75dcafaaea258ec60a91d1169cecb75
Aakash Sharma <aakash.y.sharma@gmail.com>
2015-06-29 01:27:53 +0200

    Updated HistoryManager

--- UPartyPlayer/src/main/java/de/upp/player/TrackHistoryManager.java
+++ UPartyPlayer/src/main/java/de/upp/player/TrackHistoryManager.java
@@ -9,16 +9,39 @@ import de.upp.meta.Genre;
 import de.upp.meta.TrackMetaInfo;
 
 public class TrackHistoryManager {
-private List<TrackMetaInfo> tracksPlayedSoFar = null;
-private Map<Genre,Double> genreScore = null;
+	TrackHistoryManager manager = null;
+	private List<TrackMetaInfo> tracksPlayedSoFar = null;
+	private Map<Genre, Double> genreScore = null;
 
-public TrackHistoryManager() {
-	genreScore = new HashMap<Genre, Double>();
-	tracksPlayedSoFar = new ArrayList<TrackMetaInfo>();
-	//Initializing HashMap
-	for(Genre genre: Genre.values()){
-		genreScore.put(genre, 0.0d);
+	TrackHistoryManager() {
+		genreScore = new HashMap<Genre, Double>();
+		tracksPlayedSoFar = new ArrayList<TrackMetaInfo>();
+		// Initializing HashMap
+		for (Genre genre : Genre.values()) {
+			genreScore.put(genre, 0.0d);
+		}
 	}
-}
-	
+
+	//Singleton
+	public TrackHistoryManager getInstance() {
+		if (manager == null) {
+			manager = new TrackHistoryManager();
+		}
+		return manager;
+
+	}
+
+	//Adds track, updates score
+	public void addToHistory(TrackMetaInfo track, Double trackScore) {
+		tracksPlayedSoFar.add(track);
+		//Formula to update score based on Track's genre and trackScore
+		Genre trackGenre = track.getGenre();
+		Double oldScore= this.genreScore.get(trackGenre);
+		//TODO derive a new way to maintain score.
+		Double newSccore = oldScore * trackScore / 100;
+		
+		this.genreScore.put(trackGenre, newSccore);
+		
+	}
+
 }
